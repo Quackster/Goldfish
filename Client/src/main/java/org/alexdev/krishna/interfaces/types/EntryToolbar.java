6b68df0f97d1a9e84c72fed841aa6b537f9c01f9
Palsternakka <hello@oliverdalton.co.uk>
2022-05-14 18:27:34 +0100

    Added EntryToolbar buttons/animation

--- Client/src/main/java/org/alexdev/krishna/interfaces/types/EntryToolbar.java
+++ Client/src/main/java/org/alexdev/krishna/interfaces/types/EntryToolbar.java
@@ -1,23 +1,34 @@
 package org.alexdev.krishna.interfaces.types;
 
 import org.alexdev.krishna.Movie;
+import org.alexdev.krishna.controls.ImageButton;
 import org.alexdev.krishna.controls.Label;
+import org.alexdev.krishna.game.resources.ResourceManager;
 import org.alexdev.krishna.interfaces.Interface;
 import org.alexdev.krishna.interfaces.InterfaceType;
 
 import javafx.scene.Cursor;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 public class EntryToolbar extends Interface {
     private Pane pane;
 
+    // TO-DO
+    // - handle resizing
+    // - add user icon
+
     @Override
     public void start() {
         this.pane = new Pane();
         this.pane.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
+        this.pane.setLayoutY(Movie.getInstance().getCurrentVisualiser().getPane().getHeight());
 
         var userLabel = new Label("Parsnip", "#FFFFFF");
         userLabel.setLayoutX(51);
@@ -44,7 +55,40 @@ public class EntryToolbar extends Interface {
         clubDescLabel.setCursor(Cursor.HAND);
         clubDescLabel.setOnMouseClicked(e -> Movie.getInstance().createObject(new Alert("clubDescLabel clicked")));
 
+        var clubButton = new ImageButton("sprites/entry_toolbar/club.png");
+        clubButton.setLayoutX(245);
+        clubButton.setOnMouseClicked(e -> Movie.getInstance().createObject(new Alert("clubButton clicked")));
+        
+        var chatButton = new ImageButton("sprites/entry_toolbar/chat.png");
+        chatButton.setOnMouseClicked(e -> Movie.getInstance().createObject(new Alert("chatButton clicked")));
+        chatButton.setLayoutX(676);
+        
+        var friendsButton = new ImageButton("sprites/entry_toolbar/friends.png");
+        friendsButton.setLayoutX(716);
+        friendsButton.setOnMouseClicked(e -> Movie.getInstance().createObject(new Alert("friendsButton clicked")));
+        
+        var navigatorButton = new ImageButton("sprites/entry_toolbar/navigator.png");
+        navigatorButton.setLayoutX(753);
+        navigatorButton.setOnMouseClicked(e -> Movie.getInstance().createObject(new Alert("navigatorButton clicked")));
+        
+        var eventsButton = new ImageButton("sprites/entry_toolbar/events.png");
+        eventsButton.setLayoutX(798);
+        eventsButton.setOnMouseClicked(e -> Movie.getInstance().createObject(new Alert("eventsButton clicked")));
+        
+        var catalogueButton = new ImageButton("sprites/entry_toolbar/catalogue.png");
+        catalogueButton.setLayoutX(839);
+        catalogueButton.setOnMouseClicked(e -> Movie.getInstance().createObject(new Alert("catalogueButton clicked")));
+        
+        var gamesButton = new ImageButton("sprites/entry_toolbar/games.png");
+        gamesButton.setLayoutX(879);
+        gamesButton.setOnMouseClicked(e -> Movie.getInstance().createObject(new Alert("gamesButton clicked")));
+        
+        var helpButton = new ImageButton("sprites/entry_toolbar/help.png");
+        helpButton.setLayoutX(915);
+        helpButton.setOnMouseClicked(e -> Movie.getInstance().createObject(new Alert("helpButton clicked")));
+        
         this.pane.getChildren().addAll(userLabel, mottoLabel, updateIdLabel, clubTitleLabel, clubDescLabel);
+        this.pane.getChildren().addAll(clubButton, chatButton, friendsButton, navigatorButton, eventsButton, catalogueButton, gamesButton, helpButton);
 
         Movie.getInstance().getInterfaceScheduler().receiveUpdate(this);
     }
@@ -64,6 +108,10 @@ public class EntryToolbar extends Interface {
     @Override
     public void update() {
         this.pane.setPrefSize(Movie.getInstance().getPrimaryStage().getWidth(), 55);
+
+        if (this.pane.getLayoutY() != Movie.getInstance().getCurrentVisualiser().getPane().getHeight() - this.pane.getHeight()) {
+            this.pane.setLayoutY(this.pane.getLayoutY() - 5);
+        }
     }
 
     @Override
